27e9da16bb2b724a8da5248d19ea3a46935e34f1
xedinsonx <xedinsonx2@gmail.com>
2020-09-13 18:03:39 -0500

    Python Objetos

---
+++ PythonObjetos/PythonObjetos.py
@@ -0,0 +1,9 @@
+class Cerveza:
+    def __init__(self, nombre):
+       self.nombre = nombre
+
+       def añejar(self):
+            print("Tu cerveza se ha añejado")
+
+       MiCerveza = Cerveza("Emborracharlight")
+       MiCerveza.añejar()
